c550a3c983151c9bac8961d55a504c45eb3e1bea
Brandon Hurlburt <hurlburb@microsoft.com>
2018-11-02 10:45:08 -0700

    start

--- SetDateTakenOnAndroidBurstPhotos/Program.cs
+++ SetDateTakenOnAndroidBurstPhotos/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace SetDateTakenOnAndroidBurstPhotos
 {
@@ -6,7 +8,28 @@ namespace SetDateTakenOnAndroidBurstPhotos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var targetDir = Environment.CurrentDirectory;
+            if (args.Length > 0)
+            {
+                targetDir = args[0];
+            }
+
+            var photos = GetBurstPhotoPaths(targetDir);
+
+            UpdatePhotoDateTaken(photos);
+        }
+
+        private static void UpdatePhotoDateTaken(IEnumerable<string> photos)
+        {
+            foreach (var photoFilePath in photos)
+            {
+                ExifLibrary.ExifPropertyFactory.Get()
+            }
+        }
+
+        private static IEnumerable<string> GetBurstPhotoPaths(string targetDir)
+        {
+            return Directory.EnumerateFiles(targetDir, @"*_BURST*.jpg", new EnumerationOptions { MatchCasing = MatchCasing.CaseInsensitive, RecurseSubdirectories = true });
         }
     }
 }
